e52658dbeb9b94b23928a1efef2bb43ddc3326cc
Egor Kashirin <mailgorinich@gmail.com>
2019-04-18 18:49:02 +0500

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-import "babel-polyfill";
-import Chart from "chart.js";
+import 'babel-polyfill';
+import Chart from 'chart.js';
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+const currencyURL = 'www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml';
+// const meteoURL = '/xml.meteoservice.ru/export/gismeteo/point/140.xml';
 
 async function loadCurrency() {
   const response = await fetch(currencyURL);
-  const xmlTest = await response.text();
-  const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
+  const xmlBody = await response.text();
+  const currencyData = new DOMParser().parseFromString(xmlBody, 'text/xml');
+  const rates = currencyData.querySelectorAll('Cube[currency][rate]');
   const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
+  for (const rateTag of rates) {
+    const rate = rateTag.getAttribute('rate');
+    const currency = rateTag.getAttribute('currency');
     result[currency] = rate;
   }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
+  result['EUR'] = 1;
   return result;
 }
 
@@ -32,26 +28,27 @@ function normalizeDataByCurrency(data, currency) {
   return result;
 }
 
-const buttonBuild = document.getElementById("btn");
-const canvasCtx = document.getElementById("out").getContext("2d");
-buttonBuild.addEventListener("click", async function() {
+const buttonBuild = document.getElementById('btn');
+const canvasCtx = document.getElementById('out').getContext('2d');
+
+buttonBuild.addEventListener('click', async function() {
   const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
-  );
+  const normalData = normalizeDataByCurrency(currencyData, 'RUB');
+  const keys = Object.keys(normalData).sort((k1, k2) => {
+    return normalData[k1] > normalData[k2]? 1 : -1;
+  });
   const plotData = keys.map(key => normalData[key]);
 
   const chartConfig = {
-    type: "line",
+    type: 'line',
 
     data: {
       labels: keys,
       datasets: [
         {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
+          label: 'Стоимость валюты в рублях',
+          backgroundColor: 'rgb(255, 20, 20)',
+          borderColor: 'rgb(180, 0, 0)',
           data: plotData
         }
       ]
@@ -63,15 +60,9 @@ buttonBuild.addEventListener("click", async function() {
     chart.data.datasets[0].data = chartConfig.data.datasets[0].data;
     chart.update({
       duration: 800,
-      easing: "easeOutBounce"
+      easing: 'easeOutBounce'
     });
   } else {
     window.chart = new Chart(canvasCtx, chartConfig);
   }
 });
-
-function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
-  return 0;
-}
